1a409e0e8c23cff292e34d5dfc7d42262b66a996
rauljordan <raul@prysmaticlabs.com>
2020-08-24 18:37:06 -0500

    transition completely to tailwind in sass

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,16 @@
 module.exports = {
   purge: [],
   theme: {
+    colors: {
+      primary: '#7467ef',
+      secondary: '#ff9e43',
+      error: '#e95455',
+      default: '#1a2038',
+      paper: '#222A45',
+      paperlight: '#30345b',
+      muted: 'rgba(255, 255, 255, 0.7)',
+      hint: 'rgba(255, 255, 255, 0.5)'
+    },
     extend: {},
   },
   variants: {},

5f12a992aec93373d764e2cadc28b7effb6ec511
rauljordan <raul@prysmaticlabs.com>
2020-08-25 16:38:46 -0500

    vertical stepper on small screen

--- src/app/modules/onboarding/components/hd-wallet-wizard/hd-wallet-wizard.component.ts
+++ src/app/modules/onboarding/components/hd-wallet-wizard/hd-wallet-wizard.component.ts
@@ -1,23 +1,34 @@
 import { Component, OnInit, ViewChild, NgZone } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatStepper } from '@angular/material/stepper';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { tap, takeUntil } from 'rxjs/operators';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-hd-wallet-wizard',
   templateUrl: './hd-wallet-wizard.component.html',
 })
 export class HdWalletWizardComponent implements OnInit {
+  // Properties.
   isLinear = false;
+  isSmallScreen = false;
   mnemonicFormGroup: FormGroup;
   accountsFormGroup: FormGroup;
   passwordFormGroup: FormGroup;
+
+  // View children.
   @ViewChild('stepper') stepper: MatStepper;
+
+  // Observables and subjects.
+  destroyed$ = new Subject();
 
   constructor(
     private _formBuilder: FormBuilder,
+    private breakpointObserver: BreakpointObserver
   ) {}
 
-  ngOnInit() {
+  ngOnInit(): void {
     this.mnemonicFormGroup = this._formBuilder.group({
       mnemonic: ['', Validators.required]
     });
@@ -28,5 +39,19 @@ export class HdWalletWizardComponent implements OnInit {
       password: ['', Validators.required],
       passwordConfirmation: ['', Validators.required]
     });
+    this.breakpointObserver.observe([
+      Breakpoints.XSmall,
+      Breakpoints.Small
+    ]).pipe(
+      tap(result => {
+        this.isSmallScreen = result.matches;
+      }),
+      takeUntil(this.destroyed$),
+    ).subscribe();
+  }
+
+  ngOnDestroy(): void {
+    this.destroyed$.next();
+    this.destroyed$.complete();
   }
 }

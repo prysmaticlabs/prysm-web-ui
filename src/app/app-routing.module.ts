240893e847e9121d3ad67dc7b611cccb1d749a17
rauljordan <raul@prysmaticlabs.com>
2020-08-24 19:43:50 -0500

    gains and losses page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -22,7 +22,7 @@ const routes: Routes = [
   {
     path: 'dashboard',
     component: DashboardComponent,
-    canActivate: [AuthGuard],
+    // canActivate: [AuthGuard],
     children: [
       {
         path: 'gains-and-losses',

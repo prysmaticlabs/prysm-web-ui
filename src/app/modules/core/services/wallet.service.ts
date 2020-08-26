122fb5f42e4be7978d71c07704a9caa45c00ba27
rauljordan <raul@prysmaticlabs.com>
2020-08-26 16:59:40 -0500

    share replay with explanation for generate mnemonic

--- src/app/modules/core/services/wallet.service.ts
+++ src/app/modules/core/services/wallet.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { throwError } from 'rxjs';
-import { map, catchError } from 'rxjs/operators';
+import { map, catchError, shareReplay } from 'rxjs/operators';
 
 export interface WalletResponse {
   walletPath: string;
@@ -15,14 +15,22 @@ export interface GenerateMnemonicResponse {
   providedIn: 'root'
 })
 export class WalletService {
-  token: string;
   constructor(
     private http: HttpClient,
   ) { }
 
+  // Properties.
+  token: string;
+
+  // Observables.
   walletConfig$ = this.http.get<WalletResponse>('/api/wallet');
+  // Retrieve a randomly generateed bip39 mnemonic from the backend,
+  // ensuring it can be replayed by multiple subscribers. For example: being able
+  // to verify the generated mnemonic matches the user input in a confirmation box
+  // would require share replay for us to compare to the proper value.
   generateMnemonic$ = this.http.get<GenerateMnemonicResponse>('/api/mnemonic/generate').pipe(
     map((resp: GenerateMnemonicResponse) => resp.mnemonic),
+    shareReplay(1),
     catchError(err => throwError(err)),
   );
 }

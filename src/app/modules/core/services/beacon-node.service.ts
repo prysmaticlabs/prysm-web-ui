bad49110461c78f0f2b34627661321988c51be5c
Raul Jordan <raul@prysmaticlabs.com>
2020-09-04 16:10:23 -0500

    node status polling

--- src/app/modules/core/services/beacon-node.service.ts
+++ src/app/modules/core/services/beacon-node.service.ts
@@ -1,7 +1,15 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { tap } from 'rxjs/operators';
+
+import { interval } from 'rxjs';
+import { tap, startWith, mergeMap } from 'rxjs/operators';
+
 import { EnvironmenterService } from './environmenter.service';
+import {
+  NodeConnectionResponse,
+} from 'src/app/proto/validator/accounts/v2/web_api';
+
+const POLLING_INTERVAL = 5000;
 
 @Injectable({
   providedIn: 'root',
@@ -16,9 +24,13 @@ export class BeaconNodeService {
   private beaconNodeEndpoint: string;
 
   // Observables.
-  conn$ = this.http.get<object>(`${this.apiUrl}/health/node_connection`).pipe(
-    tap((res: any)=> {
+  status$ = this.http.get<NodeConnectionResponse>(`${this.apiUrl}/health/node_connection`).pipe(
+    tap((res: NodeConnectionResponse) => {
       this.beaconNodeEndpoint = res.beaconNodeEndpoint;
     }),
+  );
+  statusPoll$ = interval(POLLING_INTERVAL).pipe(
+    startWith(0),
+    mergeMap(_ => this.status$),
   );
 }
--- src/app/modules/dashboard/components/node-status/node-status.component.ts
+++ src/app/modules/dashboard/components/node-status/node-status.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { BeaconNodeService } from 'src/app/modules/core/services/beacon-node.service';
 
+import { tap } from 'rxjs/operators';
+
 @Component({
   selector: 'app-node-status',
   templateUrl: './node-status.component.html',
@@ -9,5 +11,7 @@ import { BeaconNodeService } from 'src/app/modules/core/services/beacon-node.ser
 })
 export class NodeStatusComponent {
   constructor(private nodeService: BeaconNodeService) { }
-  nodeConnection$ = this.nodeService.conn$;
+  nodeConnection$ = this.nodeService.statusPoll$.pipe(
+    tap(() => console.log('fired')),
+  );
 }

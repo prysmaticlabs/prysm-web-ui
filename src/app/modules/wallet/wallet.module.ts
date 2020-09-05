00aee7547ba98f940f7efaa372e2e68cc33546a2
Raul Jordan <raul@prysmaticlabs.com>
2020-09-05 14:54:30 -0500

    revert changes

--- src/app/modules/wallet/wallet.module.ts
+++ src/app/modules/wallet/wallet.module.ts
@@ -6,14 +6,9 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { SharedModule } from '../shared/shared.module';
 import { AccountListComponent } from './pages/account-list/account-list.component';
 import { WalletConfigComponent } from './pages/wallet-config/wallet-config.component';
-import { AccountsCardComponent } from './components/accounts-card/accounts-card.component';
 
 @NgModule({
-  declarations: [
-    AccountListComponent,
-    WalletConfigComponent, 
-    AccountsCardComponent,
-  ],
+  declarations: [AccountListComponent, WalletConfigComponent],
   imports: [
     CommonModule,
     FormsModule,

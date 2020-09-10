73f0631be143b37b30c9b299477e9973046e0457
rauljordan <raul@prysmaticlabs.com>
2020-09-10 15:47:50 -0500

    fix up tests

--- src/app/modules/dashboard/components/activation-queue/activation-queue.component.spec.ts
+++ src/app/modules/dashboard/components/activation-queue/activation-queue.component.spec.ts
@@ -7,11 +7,13 @@ import { ActivationQueueComponent } from './activation-queue.component';
 import { ValidatorService } from 'src/app/modules/core/services/validator.service';
 import { ValidatorQueue } from 'src/app/proto/eth/v1alpha1/beacon_chain';
 import { SharedModule } from 'src/app/modules/shared/shared.module';
+import { WalletService } from 'src/app/modules/core/services/wallet.service';
 
 describe('ActivationQueueComponent', () => {
   let component: ActivationQueueComponent;
   let fixture: ComponentFixture<ActivationQueueComponent>;
   let service: ValidatorService = MockService(ValidatorService);
+  let walletService: WalletService = MockService(WalletService);
   const defaultQueueResponse = {
     churnLimit: "4" as any,
     activationPublicKeys: [
@@ -20,8 +22,12 @@ describe('ActivationQueueComponent', () => {
     activationValidatorIndices: [1],
     exitPublicKeys: [],
     exitValidatorIndices: [],
-  } as ValidatorQueue
+  } as ValidatorQueue;
+  const defaultKeysResponse = [
+    new Uint8Array([1, 2, 3]),
+  ];
   service['activationQueue$'] = of(defaultQueueResponse);
+  walletService['validatingPublicKeys$'] = of(defaultKeysResponse);
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -31,6 +37,7 @@ describe('ActivationQueueComponent', () => {
       ],
       providers: [
         { provide: ValidatorService, useValue: service },
+        { provide: WalletService, useValue: walletService },
       ]
     })
     .compileComponents();

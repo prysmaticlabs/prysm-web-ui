b1eb9b5f4c25e02ef1e231e6d40357922a71955d
rauljordan <raul@prysmaticlabs.com>
2020-09-10 11:04:33 -0500

    queue into sets

--- src/app/modules/dashboard/components/activation-queue/activation-queue.component.ts
+++ src/app/modules/dashboard/components/activation-queue/activation-queue.component.ts
@@ -1,6 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ValidatorService } from 'src/app/modules/core/services/validator.service';
-import { take, tap } from 'rxjs/operators';
+import { ValidatorQueue } from 'src/app/proto/eth/v1alpha1/beacon_chain';
+import { map, takeUntil, tap } from 'rxjs/operators';
+import { Subject } from 'rxjs';
+
+interface QueueData {
+  churnLimit: number;
+  activationIndices: Set<number>;
+  activationPublicKeys: Set<Uint8Array>;
+  exitIndices: Set<number>;
+  exitPublicKeys: Set<Uint8Array>;
+}
 
 @Component({
   selector: 'app-activation-queue',
@@ -8,15 +18,47 @@ import { take, tap } from 'rxjs/operators';
   styles: [
   ]
 })
-export class ActivationQueueComponent implements OnInit {
+export class ActivationQueueComponent implements OnInit, OnDestroy {
   constructor(
     private validatorService: ValidatorService,
   ) { }
+  
+  private destroyed$$ = new Subject<void>();
+  private queueData: QueueData;
 
   ngOnInit(): void {
     this.validatorService.activationQueue$.pipe(
-      tap(console.log),
-      take(1),
+      map(this.transformData.bind(this)),
+      tap((queueData: QueueData) => this.queueData = queueData),
+      takeUntil(this.destroyed$$),
     ).subscribe();
+  }
+
+  ngOnDestroy(): void {
+    this.destroyed$$.next();
+    this.destroyed$$.complete();
+  }
+
+  transformData(queue: ValidatorQueue): QueueData {
+    const activationKeysSet = new Set<Uint8Array>();
+    const activationIndicesSet = new Set<number>();
+    const exitKeysSet = new Set<Uint8Array>();
+    const exitIndicesSet = new Set<number>();
+
+    queue.activationPublicKeys.forEach((key, idx) => {
+      activationKeysSet.add(key);
+      activationIndicesSet.add(queue.activationValidatorIndices[idx]);
+    });
+    queue.exitPublicKeys.forEach((key, idx) => {
+      exitKeysSet.add(key);
+      exitIndicesSet.add(queue.exitValidatorIndices[idx]);
+    });
+    return {
+      churnLimit: queue.churnLimit,
+      activationIndices: activationIndicesSet,
+      activationPublicKeys: activationKeysSet,
+      exitIndices: exitIndicesSet,
+      exitPublicKeys: exitKeysSet,
+    };
   }
 }

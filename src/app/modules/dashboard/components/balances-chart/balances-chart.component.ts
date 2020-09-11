630196dba372b2117d6248b630b133b4889d0d37
rauljordan <raul@prysmaticlabs.com>
2020-09-11 16:58:26 -0500

    graph works

--- src/app/modules/dashboard/components/balances-chart/balances-chart.component.ts
+++ src/app/modules/dashboard/components/balances-chart/balances-chart.component.ts
@@ -1,11 +1,10 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ValidatorService } from 'src/app/modules/core/services/validator.service';
 import { BeaconNodeService } from 'src/app/modules/core/services/beacon-node.service';
-import { SLOTS_PER_EPOCH, MILLISECONDS_PER_SLOT } from 'src/app/modules/core/constants';
+import { SLOTS_PER_EPOCH, MILLISECONDS_PER_SLOT, GWEI_PER_ETHER } from 'src/app/modules/core/constants';
 import { switchMap, takeUntil } from 'rxjs/operators';
 import { ChainHead, ValidatorBalances } from 'src/app/proto/eth/v1alpha1/beacon_chain';
 import { Subject, zip } from 'rxjs';
-import { WHITE_ON_BLACK_CSS_CLASS } from '@angular/cdk/a11y/high-contrast-mode/high-contrast-mode-detector';
 
 @Component({
   selector: 'app-balances-chart',
@@ -38,49 +37,35 @@ export class BalancesChartComponent implements OnInit, OnDestroy {
     this.destroyed$$.complete();
   }
 
-  randomData() {
-    const now = new Date();
-    const value = Math.random() * 21 - 10;
-    return {
-      name: now.toString(),
-      value: [
-        [now.getFullYear(), now.getMonth() + 1, now.getDate()].join('/'),
-        Math.round(value)
-      ]
-    };
-  }
-
   updateData(genesisTime: number, balances: ValidatorBalances[]): void {
     console.log(balances);
     console.log(genesisTime);
     const xAxisData = [];
     const data1 = [];
-    const data2 = [];
 
+    const avgBalances: number[] = [];
     for (let i = 0; i < balances.length; i++) {
       let epoch = balances[i].epoch
       let totalMilliseconds = epoch * MILLISECONDS_PER_SLOT * SLOTS_PER_EPOCH
-      console.log(totalMilliseconds);
-      let now = new Date(genesisTime*1000 + totalMilliseconds);
+      let timeSinceGenesis = new Date(genesisTime*1000 + totalMilliseconds);
+      const pureBalances = balances[i].balances.map(b => b.balance / GWEI_PER_ETHER);
+      const total = pureBalances.reduce((prev, curr) => prev + curr, 0);
+      const avg = total / pureBalances.length;
+      avgBalances.push(avg);
       data1.push({
-        name: now.toString(),
+        name: timeSinceGenesis.toString(),
         value: [
-          now,
-          balances[i].balances[0].balance,
-        ]
-      });
-      data2.push({
-        name: now.toString(),
-        value: [
-          now,
-          balances[i].balances[0].balance + (0.02 * i),
+          timeSinceGenesis,
+          avg, 
         ]
       });
     }
+    const globalMin = Math.min(...avgBalances);
+    const globalMax = Math.max(...avgBalances);
 
     this.options = {
       legend: {
-        data: ['bar'],
+        data: ['Average balance'],
         align: 'left',
         textStyle: {
           color: 'white',
@@ -112,15 +97,15 @@ export class BalancesChartComponent implements OnInit, OnDestroy {
       },
       color: [
         "#7467ef",
-        "#ff9e43",
+        // "#ff9e43",
       ],
       yAxis: {
         type: 'value',
         splitLine: {
           show: false
         },
-        min: '32.27',
-        max: '32.4',
+        min: globalMin,
+        max: globalMax,
         axisLine: {
           lineStyle: {
             color: 'white',
@@ -134,12 +119,12 @@ export class BalancesChartComponent implements OnInit, OnDestroy {
           data: data1,
           animationDelay: (idx: number) => idx * 10,
         },
-        {
-          name: 'Range balance',
-          type: 'line',
-          data: data2,
-          animationDelay: (idx: number) => idx * 20,
-        },
+        // {
+        //   name: 'Range balance',
+        //   type: 'line',
+        //   data: data2,
+        //   animationDelay: (idx: number) => idx * 20,
+        // },
       ],
       animationEasing: 'elasticOut',
       animationDelayUpdate: (idx: number) => idx * 5,

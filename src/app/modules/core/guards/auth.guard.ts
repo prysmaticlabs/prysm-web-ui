0dc92f085c8f0d341a88e989b5d69d1fb4cd4fc4
Raul Jordan <raul@prysmaticlabs.com>
2020-09-17 14:46:11 -0500

    disable auth guard if not prod (#39)

--- src/app/modules/core/guards/auth.guard.ts
+++ src/app/modules/core/guards/auth.guard.ts
@@ -9,19 +9,22 @@ import {
 import { Observable } from 'rxjs';
 
 import { AuthenticationService } from '../services/authentication.service';
+import { EnvironmenterService } from '../services/environmenter.service';
 
 @Injectable({ providedIn: 'root' })
 export class AuthGuard implements CanActivate {
   constructor(
     private authenticationService: AuthenticationService,
     private router: Router,
+    private environmenter: EnvironmenterService,
   ) { }
 
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
   ): Observable<any> | Promise<boolean|UrlTree> | boolean{
-    if (this.authenticationService.token) {
+    const isDev = !this.environmenter.env.production;
+    if (this.authenticationService.token || isDev) {
       return true;
     }
     this.router.navigate(['/']);

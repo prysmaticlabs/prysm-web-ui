c533343a9f9efecf298111c92a1633f8ae0bccd0
Raul Jordan <raul@prysmaticlabs.com>
2020-10-03 17:01:38 -0500

    passing tests

--- src/app/modules/system-process/services/logs.service.spec.ts
+++ src/app/modules/system-process/services/logs.service.spec.ts
@@ -1,16 +1,22 @@
-// import { TestBed } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
+import { EnvironmenterService } from '../../core/services/environmenter.service';
 
-// import { LogsService } from './logs.service';
+import { LogsService } from './logs.service';
 
-// describe('LogsService', () => {
-//   let service: LogsService;
+describe('LogsService', () => {
+  let service: LogsService;
 
-//   beforeEach(() => {
-//     TestBed.configureTestingModule({});
-//     service = TestBed.inject(LogsService);
-//   });
+  beforeEach(() => {
+    const envSpy = jasmine.createSpyObj('EnvironmenterService', ['env']);
+    TestBed.configureTestingModule({
+      providers: [
+        { provide: EnvironmenterService, useValue: envSpy },
+      ]
+    });
+    service = TestBed.inject(LogsService);
+  });
 
-//   it('should be created', () => {
-//     expect(service).toBeTruthy();
-//   });
-// });
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});

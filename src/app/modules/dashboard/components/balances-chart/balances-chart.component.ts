f6a4e104452bd03df2cfe8dc3434122633c89d01
Raul Jordan <raul@prysmaticlabs.com>
2020-10-05 17:41:15 -0500

    fix up text

--- src/app/modules/dashboard/components/balances-chart/balances-chart.component.ts
+++ src/app/modules/dashboard/components/balances-chart/balances-chart.component.ts
@@ -3,13 +3,26 @@ import { ValidatorService } from 'src/app/modules/core/services/validator.servic
 import { BeaconNodeService } from 'src/app/modules/core/services/beacon-node.service';
 import { SLOTS_PER_EPOCH, MILLISECONDS_PER_SLOT, GWEI_PER_ETHER } from 'src/app/modules/core/constants';
 import { switchMap, takeUntil, tap } from 'rxjs/operators';
-import { ChainHead, ValidatorBalances } from 'src/app/proto/eth/v1alpha1/beacon_chain';
+import { ChainHead, ValidatorBalances, ValidatorBalances_Balance } from 'src/app/proto/eth/v1alpha1/beacon_chain';
 import { Subject, zip } from 'rxjs';
 import { BigNumber } from 'ethers';
 import * as moment from 'moment';
 
 const EPOCH_LOOKBACK = 4;
 const NUM_ACCOUNTS = 10;
+
+interface EpochBalances {
+  lowestBalances: BigNumber[];
+  avgBalances: BigNumber[];
+  highestBalances: BigNumber[];
+  timestamps: string[];
+}
+
+interface EpochDeltas {
+  lowestDeltas: number[];
+  avgDeltas: number[];
+  highestDeltas: number[];
+}
 
 @Component({
   selector: 'app-balances-chart',
@@ -44,55 +57,12 @@ export class BalancesChartComponent implements OnInit, OnDestroy {
   }
 
   updateData(genesisTime: number, balances: ValidatorBalances[]): void {
-    console.log(balances);
-    const xAxisData = [];
-    const lowest: BigNumber[] = [];
-    const highest: BigNumber[] = [];
-    const avgBalances: BigNumber[] = [];
-
-    for (let i = 0; i < balances.length; i++) {
-      const epoch = balances[i].epoch;
-      const totalMilliseconds = BigNumber.from(epoch).mul(SLOTS_PER_EPOCH).mul(MILLISECONDS_PER_SLOT);
-      const genesisTimeMilliseconds = genesisTime * 1000;
-      const millisecondsSinceGenesis = totalMilliseconds.add(genesisTimeMilliseconds);
-      const epochTimestamp = new Date(millisecondsSinceGenesis.toNumber());
-
-      const pureBalances = balances[i].balances.map(b => BigNumber.from(b.balance));
-      const total = pureBalances.reduce((prev, curr) => prev.add(curr), BigNumber.from('0'));
-      const avg = total.div(pureBalances.length);
-      const lowestBal = this.minbigNum(pureBalances);
-      const highestBal = this.maxBigNum(pureBalances);
-
-      const formatted = moment(epochTimestamp).format('hh:mm:ss');
-      xAxisData.push(`epoch ${epoch} ${formatted}`);
-      avgBalances.push(avg);
-      lowest.push(lowestBal);
-      highest.push(highestBal);
-    }
-
-    // Compute deltas.
-    const avgDelta: number[] = [];
-    const lowestDelta: number[] = [];
-    const highestDelta: number[] = [];
-    for (let i = 0; i < (avgBalances.length - 1); i++) {
-      const avgCurr = avgBalances[i];
-      const avgNext = avgBalances[i + 1];
-      console.log(avgNext.sub(avgCurr).toString());
-      avgDelta.push(avgNext.sub(avgCurr).toNumber() / GWEI_PER_ETHER);
-
-      const lowCurr = lowest[i];
-      const lowNext = lowest[i + 1];
-      lowestDelta.push(lowNext.sub(lowCurr).toNumber() / GWEI_PER_ETHER);
-
-      const highCurr = highest[i];
-      const highNext = highest[i + 1];
-      highestDelta.push(highNext.sub(highCurr).toNumber() / GWEI_PER_ETHER);
-    }
-    const xData = xAxisData.slice(1, xAxisData.length);
-
+    const bals = this.computeEpochBalances(genesisTime, balances);
+    const deltas = this.computeEpochDeltas(bals);
+    const xAxisData = bals.timestamps.slice(1, bals.timestamps.length);
     this.options = {
       legend: {
-        data: ['Lowest ETH gains', 'Average ETH gains', 'Highest ETH gains'],
+        data: ['Worst performing validator', 'Average performing validator', 'Best performing validator'],
         align: 'left',
         textStyle: {
           color: 'white',
@@ -120,7 +90,7 @@ export class BalancesChartComponent implements OnInit, OnDestroy {
         }
       },
       xAxis: {
-        data: xData,
+        data: xAxisData,
         axisLabel: {
           color: 'white',
           fontSize: 14,
@@ -153,29 +123,85 @@ export class BalancesChartComponent implements OnInit, OnDestroy {
       },
       series: [
         {
-          name: 'Lowest ETH gains',
+          name: 'Worst performing validator',
           type: 'bar',
           barGap: 0,
-          data: lowestDelta,
+          data: deltas.lowestDeltas,
           animationDelay: (idx: number) => idx * 10,
         },
         {
-          name: 'Average ETH gains',
+          name: 'Average performing validator',
           type: 'bar',
           barGap: 0,
-          data: avgDelta,
+          data: deltas.avgDeltas,
           animationDelay: (idx: number) => idx * 10,
         },
         {
-          name: 'Highest ETH gains',
+          name: 'Best performing validator',
           type: 'bar',
           barGap: 0,
-          data: highestDelta,
+          data: deltas.highestDeltas,
           animationDelay: (idx: number) => idx * 10,
         },
       ],
       animationEasing: 'elasticOut',
       animationDelayUpdate: (idx: number) => idx * 5,
+    };
+  }
+
+  private computeEpochBalances(
+    genesisTime: number, balances: ValidatorBalances[]
+  ): EpochBalances {
+    const lowestBalances: BigNumber[] = [];
+    const highestBalances: BigNumber[] = [];
+    const avgBalances: BigNumber[] = [];
+    const timestamps: string[] = [];
+
+    for (let i = 0; i < balances.length; i++) {
+      const epoch = balances[i].epoch;
+      const epochTimestamp = this.computeEpochTimestamp(genesisTime, epoch);
+
+      const pureBalances = balances[i].balances.map(b => BigNumber.from(b.balance));
+      const total = pureBalances.reduce((prev, curr) => prev.add(curr), BigNumber.from('0'));
+      const avg = total.div(pureBalances.length);
+      const lowestBal = this.minbigNum(pureBalances);
+      const highestBal = this.maxBigNum(pureBalances);
+
+      const formatted = moment(epochTimestamp).format('hh:mm:ss');
+      timestamps.push(`epoch ${epoch} ${formatted}`);
+      avgBalances.push(avg);
+      lowestBalances.push(lowestBal);
+      highestBalances.push(highestBal);
+    }
+    return {
+      lowestBalances,
+      avgBalances,
+      highestBalances,
+      timestamps,
+    };
+  }
+
+  private computeEpochDeltas(bals: EpochBalances): EpochDeltas {
+    const lowestDeltas: number[] = [];
+    const avgDeltas: number[] = [];
+    const highestDeltas: number[] = [];
+    for (let i = 0; i < (bals.avgBalances.length - 1); i++) {
+      const lowCurr = bals.lowestBalances[i];
+      const lowNext = bals.lowestBalances[i + 1];
+      lowestDeltas.push(lowNext.sub(lowCurr).toNumber() / GWEI_PER_ETHER);
+
+      const avgCurr = bals.avgBalances[i];
+      const avgNext = bals.avgBalances[i + 1];
+      avgDeltas.push(avgNext.sub(avgCurr).toNumber() / GWEI_PER_ETHER);
+
+      const highCurr = bals.highestBalances[i];
+      const highNext = bals.highestBalances[i + 1];
+      highestDeltas.push(highNext.sub(highCurr).toNumber() / GWEI_PER_ETHER);
+    }
+    return {
+      lowestDeltas,
+      avgDeltas,
+      highestDeltas,
     };
   }
 
@@ -203,5 +229,12 @@ export class BalancesChartComponent implements OnInit, OnDestroy {
       }
     }
     return max;
+  }
+
+  private computeEpochTimestamp(genesisTime: number, epoch: string): Date {
+    const totalMilliseconds = BigNumber.from(epoch).mul(SLOTS_PER_EPOCH).mul(MILLISECONDS_PER_SLOT);
+    const genesisTimeMilliseconds = genesisTime * 1000;
+    const millisecondsSinceGenesis = totalMilliseconds.add(genesisTimeMilliseconds);
+    return new Date(millisecondsSinceGenesis.toNumber());
   }
 }

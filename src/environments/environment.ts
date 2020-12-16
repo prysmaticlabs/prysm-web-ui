87aca3d6f652e0379612b1d277c0ce3935aaf528
rauljordan <raul@prysmaticlabs.com>
2020-12-16 15:05:30 -0600

    begin

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@ import { IEnvironment } from './token';
 
 export const environment: IEnvironment = {
   production: false,
-  validatorEndpoint: 'http://localhost:7500/v2/validator',
+  validatorEndpoint: 'http://127.0.0.1:7500/api/v2/validator',
 };
 
 /*

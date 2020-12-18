d0f2d3d9da6ef7e4ba61865b2430baa4279591f9
Mohamed Mansour <mmansour@microsoft.com>
2020-12-17 20:03:12 -0800

    Redirect the user back to Login page if unauthorized
    Angular HTTP Client doesn't let you issue another request when
    requesting the next URL unless you add it to the queue, which is
    not what we are doing. So instead, if the service is coming back
    as 401, we can redirect the user back automatically to the login
    page with the return URL in tact.
    
    The return URL is needed to create a positive experience instead
    navigating back where they are coming from after logging back in.
    
    Fixes #109
    Part of #116

--- src/app/modules/core/interceptors/error.interceptor.ts
+++ src/app/modules/core/interceptors/error.interceptor.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor, HttpErrorResponse } from '@angular/common/http';
+import { Router } from '@angular/router';
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 
@@ -9,18 +10,23 @@ import { ErrorService } from '../services/error.service';
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
   constructor(
+    private router: Router,
     private authenticationService: AuthenticationService,
     private errorService: ErrorService,
   ) { }
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     return next.handle(request).pipe(catchError((err: HttpErrorResponse) => {
-      if (err.status === 401) {
-        // Auto logout if a 401 response (Unauthorized)
-        // is returned from the api.
-        this.authenticationService.logout();
-      }
       this.errorService.handleHTTPError(err);
+      if (err.status === 401) {
+        // Auto logout if a 401 response (Unauthorized) is returned from the api.
+        this.authenticationService.clearCredentials();
+        this.router.navigate(['/login'],  {
+          queryParams: {
+            returnUrl: this.router.url
+          }
+        });
+      }
       return throwError(err);
     }));
   }

c133fc9cb42cd0e3a3fe43bd05a727c758a3e781
Raul Jordan <raul@prysmaticlabs.com>
2021-01-27 20:55:42 -0600

    remove unnecessary interceptors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 
-import { ErrorInterceptor } from './modules/core/interceptors/error.interceptor';
-import { JwtInterceptor } from './modules/core/interceptors/jwt.interceptor';
 import { ENVIRONMENT } from '../environments/token';
 import { environment } from '../environments/environment';
 
@@ -35,8 +33,6 @@ import { NgxSkeletonLoaderModule } from 'ngx-skeleton-loader';
     SecurityModule,
   ],
   providers: [
-    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
-    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
     { provide: HTTP_INTERCEPTORS, useClass: MockInterceptor, multi: true },
     { provide: ENVIRONMENT, useValue: environment },
   ],
--- src/app/modules/core/services/authentication.service.ts
+++ src/app/modules/core/services/authentication.service.ts
@@ -1,8 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { switchMap, tap } from 'rxjs/operators';
-import { Observable, EMPTY } from 'rxjs';
-import { Router } from '@angular/router';
+import { tap } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 import { EnvironmenterService } from './environmenter.service';
 import { AuthRequest, AuthResponse, ChangePasswordRequest, HasUsedWebResponse } from 'src/app/proto/validator/accounts/v2/web_api';
 
@@ -12,11 +11,9 @@ import { AuthRequest, AuthResponse, ChangePasswordRequest, HasUsedWebResponse } 
 export class AuthenticationService {
   constructor(
     private http: HttpClient,
-    private router: Router,
     private environmenter: EnvironmenterService,
   ) {
   }
-  token = '';
   hasSignedUp = false;
   private apiUrl = this.environmenter.env.validatorEndpoint;
 
@@ -25,11 +22,7 @@ export class AuthenticationService {
   }
 
   signup(request: AuthRequest): Observable<AuthResponse> {
-    return this.http.post<AuthResponse>(`${this.apiUrl}/signup`, request).pipe(
-      tap((res: AuthResponse) => {
-        this.token = res.token;
-      }),
-    );
+    return this.http.post<AuthResponse>(`${this.apiUrl}/signup`, request);
   }
 
   changeUIPassword(request: ChangePasswordRequest): Observable<void> {
@@ -45,25 +38,6 @@ export class AuthenticationService {
   // Authenticate the user with a password and extract the JWT token
   // from the response object. Uses take to prevent multiple calls to the backend.
   authenticate(method: string, password: string): Observable<AuthResponse> {
-    return this.http.post<AuthResponse>(method, { password } as AuthRequest).pipe(
-      tap((res: AuthResponse) => {
-        this.token = res.token;
-      }),
-    );
-  }
-
-  // Logout the user and navigate to the application root.
-  logout(): void {
-    this.clearCredentials();
-    this.http.post<unknown>(`${this.apiUrl}/logout`, null).pipe(
-      tap(() => {
-        this.router.navigateByUrl('/');
-      }),
-      switchMap(_ => EMPTY),
-    );
-  }
-
-  clearCredentials(): void {
-    this.token = '';
+    return this.http.post<AuthResponse>(method, { password } as AuthRequest);
   }
 }

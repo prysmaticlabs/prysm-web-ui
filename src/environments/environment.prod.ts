f62e34993d8af503aeb9a928b689c68b3bb4680b
rauljordan <raul@prysmaticlabs.com>
2021-02-23 10:41:22 -0600

    simplify wallet options

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,6 @@ import { IEnvironment } from './token';
 
 export const environment: IEnvironment = {
   production: true,
-  validatorEndpoint: '/api/v2/validator',
+  // validatorEndpoint: '/api/v2/validator',
+  validatorEndpoint: 'http://127.0.0.1:7500/api/v2/validator',
 };

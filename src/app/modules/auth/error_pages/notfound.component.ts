772e66173f5c84ed9b5e68056b110bf84bb71cb3
jameshe <james@prysmaticlabs.com>
2021-09-24 14:49:47 -0400

    adding in simple 404 page

---
+++ src/app/modules/auth/error_pages/notfound.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-notfound',
+    templateUrl: 'notfound.component.html'
+})
+
+export class NotFoundComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+}

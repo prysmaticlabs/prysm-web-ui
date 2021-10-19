3a2d89badd6296ca5760c2b5777901ff811def6c
james-prysm <90280386+james-prysm@users.noreply.github.com>
2021-10-19 16:08:00 -0400

    Global dialog service (#200)
    * including global dialog service, fixing unit test, and including shared module to core module
    
    * removing extra space
    
    * Update src/app/modules/core/components/global-dialog/global-dialog.component.ts
    
    Co-authored-by: Raul Jordan <raul@prysmaticlabs.com>

---
+++ src/app/modules/core/components/global-dialog/global-dialog.component.ts
@@ -0,0 +1,33 @@
+import { HttpErrorResponse } from '@angular/common/http';
+import { Component, Inject, OnInit } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { DialogConfig, DialogConfigMessage, DialogContentAlert } from './model/interfaces';
+
+@Component({
+    selector: 'app-global-dialog',
+    templateUrl: 'global-dialog.component.html'
+})
+export class GlobalDialogComponent implements OnInit {
+    constructor(@Inject(MAT_DIALOG_DATA) public data: DialogConfigMessage) { }
+
+    title = '';
+    content = '';
+
+    alert: DialogContentAlert | null | undefined = null;
+
+    ngOnInit(): void {
+        const dialogConfig: DialogConfig = this.data.payload;
+        this.title = dialogConfig.title;
+        this.content = dialogConfig.content;
+        this.alert = dialogConfig.alert;
+
+        console.log(dialogConfig.alert);
+    }
+
+    isInstanceOfError(): boolean {
+        if (this.alert){
+            return this.alert.message instanceof Error || this.alert.message instanceof HttpErrorResponse;
+        }
+        return false;
+    }
+}
--- src/app/modules/dashboard/components/version/version.component.spec.ts
+++ src/app/modules/dashboard/components/version/version.component.spec.ts
@@ -1,9 +1,13 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MockService } from 'ng-mocks';
+import { of } from 'rxjs';
+import { ValidatorService } from 'src/app/modules/core/services/validator.service';
 import { SharedModule } from '../../../shared/shared.module';
 import { VersionComponent } from './version.component';
 
 describe('VersionComponent', () => {
+  const validatorService = MockService(ValidatorService);
   let component: VersionComponent;
   let fixture: ComponentFixture<VersionComponent>;
 
@@ -13,6 +17,9 @@ describe('VersionComponent', () => {
         SharedModule,
         BrowserAnimationsModule
       ],
+      providers: [
+        {provide: ValidatorService, useValue: validatorService}
+      ],
       declarations: [
         VersionComponent,
       ]
@@ -21,6 +28,7 @@ describe('VersionComponent', () => {
   }));
 
   beforeEach(() => {
+    validatorService.version$ = of({beacon: 'test', validator: 'test'});
     fixture = TestBed.createComponent(VersionComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();

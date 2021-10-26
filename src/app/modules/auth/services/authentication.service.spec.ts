ad43dc22a43f7105fd49248f4365d87d3011cc9c
jameshe <james@prysmaticlabs.com>
2021-10-26 09:28:07 -0400

    fixing build

--- src/app/modules/auth/services/authentication.service.spec.ts
+++ src/app/modules/auth/services/authentication.service.spec.ts
@@ -1,10 +1,8 @@
-import { TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
-import { AuthenticationService } from './authentication.service';
+import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
-import { AuthRequest, AuthResponse } from 'src/app/proto/validator/accounts/v2/web_api';
 import { EnvironmenterService } from '../../core/services/environmenter.service';
-import { MatDialog } from '@angular/material/dialog';
+import { AuthenticationService } from './authentication.service';
 
 describe('AuthenticationService', () => {
   let environmenter: EnvironmenterService;
@@ -20,8 +18,7 @@ describe('AuthenticationService', () => {
         ],
         providers: [
           AuthenticationService,
-          { provide: EnvironmenterService, useValue: serviceSpy },
-          { provide: MatDialog, useValue: {} },
+          { provide: EnvironmenterService, useValue: serviceSpy }
         ]
     });
     environmenter = TestBed.inject(EnvironmenterService);
@@ -31,26 +28,5 @@ describe('AuthenticationService', () => {
 
   afterEach(() => {
     httpMock.verify();
-  });
-
-  it('should be able to authenticate and set a token on completion', () => {
-    const mockResponse: AuthResponse = {
-      token: 'eth2',
-      tokenExpiration: 0,
-    };
-    const mockRequest: AuthRequest = {
-      password: 'password',
-      passwordConfirmation: 'password',
-    };
-    service.login(mockRequest).subscribe(resp => {
-      // Check the token in the response.
-      expect(resp.token).toEqual(mockResponse.token);
-      // Expect the token property of the service has been set
-      // to the token in the response.
-      expect(service.shortLivedToken).toEqual(resp.token);
-    });
-    const request = httpMock.expectOne(`${environmenter.env.validatorEndpoint}/login`);
-    expect(request.request.method).toBe('POST');
-    request.flush(mockResponse);
   });
 });

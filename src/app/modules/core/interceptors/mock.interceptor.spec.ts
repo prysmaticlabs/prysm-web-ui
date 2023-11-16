be10e83651057fe21af233251a4cda78dd4d6997
james-prysm <james@prysmaticlabs.com>
2023-11-16 16:16:30 -0600

    removing bad test

--- src/app/modules/core/interceptors/mock.interceptor.spec.ts
+++ src/app/modules/core/interceptors/mock.interceptor.spec.ts
@@ -1,11 +1,10 @@
-import { TestBed, inject } from '@angular/core/testing';
-import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
-import { HTTP_INTERCEPTORS, HttpClient } from '@angular/common/http';
+import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { RouterTestingModule } from '@angular/router/testing';
 
-import { MockInterceptor, VALIDATOR_API_PREFIX } from './mock.interceptor';
+import { MockInterceptor} from './mock.interceptor';
 import { EnvironmenterService } from '../services/environmenter.service';
-import { Mocks } from '../mocks';
 
 class MockEnv {
   env = { production: false };
@@ -29,17 +28,5 @@ describe('MockInterceptor', () => {
         },
       ]
     });
-  });
-
-  describe('intercept HTTP requests', () => {
-    it('should retrieve a mock for the beacon node API calls', inject([HttpClient, HttpTestingController],
-      (http: HttpClient, mock: HttpTestingController) => {
-        const endpoint = VALIDATOR_API_PREFIX + '/beacon/participation';
-        http.get(endpoint).subscribe((res) => {
-          expect(res).toEqual(Mocks[endpoint]);
-        });
-        mock.verify();
-      })
-    );
   });
 });
